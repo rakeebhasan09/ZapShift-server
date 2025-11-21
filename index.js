306576b1d1366ae785360ca2406ca70d08e77f3c
rakeebhasan <mdrakeebhasan1@gmail.com>
2025-11-21 17:10:23 +0600

    ZapShift Server setup

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("ZapShift Server is running soomthly!");
+});
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`);
+});
